e1775662414c926c6a4adb060a3e22663a4c61fc
EuTuEl <65165319+EuTuEl@users.noreply.github.com>
2024-04-25 15:52:57 +0300

    Update add-car-script.js

--- add-car-script.js
+++ add-car-script.js
@@ -1,3 +1,6 @@
+import getCookie from "./assets/helper.js";
+
+
 const form = document.getElementById("add-car-form");
 
 const signInLink = document.querySelector("#sign-in");
@@ -5,7 +8,7 @@ const logInLink = document.querySelector("#log-in");
 const logOutLink = document.querySelector("#log-out");
 const logOutButton = logOutLink.firstElementChild;
 
-if (sessionStorage.getItem("jwtToken")) {
+if (getCookie("jwt")) {
     logOutLink.classList.remove("hidden");
 
     logInLink.classList.add("hidden");
@@ -50,8 +53,11 @@ async function submitForm(event) {
 
     if(!payload) return;
 
+    const jwt =getCookie("jwt");
+    const username =getCookie("username")
+
     const response = await $.ajax("http://localhost/test-api/api/index.php/post",
-        {data: {"payload" : payload, "username" : sessionStorage.getItem("username"), "token" : sessionStorage.getItem("jwtToken")}, method: "POST"});
+        {data: {"payload" : payload, "username" : username, "token" : jwt}, method: "POST"});
 
     if (response.success) {
         alert("Success");
@@ -63,7 +69,7 @@ async function submitForm(event) {
 form.addEventListener("submit", submitForm);
 
 logOutButton.addEventListener("click", () => {
-    sessionStorage.removeItem("jwtToken");
-    sessionStorage.removeItem("username");
+    // sessionStorage.removeItem("jwtToken");
+    // sessionStorage.removeItem("username");
     window.location.reload();
 })
